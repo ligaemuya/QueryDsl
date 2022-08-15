59221eec752f4d0353f94c1fe9eef41f7562e9dd
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-08-15 20:10:44 +0900

    model

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,23 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+}

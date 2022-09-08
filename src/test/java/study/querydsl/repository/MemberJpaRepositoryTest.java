bdc2312ba3b4e9c618053f175538779c70b24c50
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-09-09 00:50:02 +0900

    수정

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -107,6 +107,7 @@ class MemberJpaRepositoryTest {
         em.persist(member1);
         em.persist(member2);
         em.persist(member3);
+
         em.persist(member4);
 
         //when
@@ -122,4 +123,39 @@ class MemberJpaRepositoryTest {
         //then
     }
 
+
+    @Test
+    public void searchTest3() throws Exception {
+        //given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+
+        em.persist(member4);
+
+        //when
+        MemberSearchCondition condition = new MemberSearchCondition();
+
+        condition.setAgeGoe(10);
+        condition.setAgeGoe(40);
+        condition.setTeamName("teamA");
+
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
+
+        assertThat(result).extracting("username").containsExactly("member2");
+        assertThat(result).extracting("username").containsExactly("member1");
+        assertThat(result).extracting("username").containsExactly("member3");
+        //then
+    }
 }
